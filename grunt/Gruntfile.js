108c90eb19231768351a4b49516254510747c360
Frederick Rosales <frederick.rosales@gmail.com>
2015-02-02 23:12:05 +0400

    mew preloader

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -17,6 +17,16 @@ module.exports = function(grunt) {
       all: [ '../js/main.js']
     },
 
+    processhtml:{
+      options: {
+       data:{
+        message: 'hello world!'
+       }
+      },
+      dist:{
+        files:{'../index.html':['../dest/index.html']}
+      }
+    },
     uglify:{
       options:{
         banner:'/*\n<%= pkg.name %> minified version 01-29-15 <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
@@ -28,7 +38,9 @@ module.exports = function(grunt) {
           '../js/classie.min.js':'../js/classie.js',
           '../js/freelancer.min.js':'../js/freelancer.js',
           '../js/owl.carousel.min.js':'../js/owl.carousel.js',
-          '../js/jquery.min.js':'../js/jquery-1.10.2.js'
+          '../js/jquery.min.js':'../js/jquery-1.10.2.js',
+          '../js/modernizr.min.js':'../js/modernizr.custom.25376.js',
+          '../js/jquery.scrollmagic.min.js':'../js/jquery.scrollmagic.js'
         }
       }
     },
@@ -44,15 +56,13 @@ module.exports = function(grunt) {
         files: {
            '../css/bootstrap.min.css':'../css/bootstrap.css',
            '../css/freelancer.min.css':'../css/freelancer.css',
-           '../css/component-min.css':'../css/component.css',
+           '../css/component.min.css':'../css/component.css',
            '../css/owl.carousel.min.css':'../css/owl.carousel.css',
-           '../css/owl.theme.min.css':'../css/owl.theme.css'
+           '../css/owl.theme.min.css':'../css/owl.theme.css',
+           '../css/pace-theme-barber-shop.min.css':'../css/pace-theme-barber-shop.css'
         }
       }
     },
-
-    
-
     // configure watch to auto update ------------------------------------------
     watch: {
       stylesheets: {
@@ -74,9 +84,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-processhtml');
   // ===========================================================================
   // CREATE TASKS ==============================================================
   // ===========================================================================
   grunt.registerTask('default', ['jshint','uglify', 'cssmin']);
+   grunt.registerTask('production', ['processhtml']);
 
 };
--- js/main.js
+++ js/main.js
@@ -3,52 +3,9 @@ var isiPad = navigator.userAgent.match(/iPad/i) !== null;
 if(isiPad){$("section.success").css("background-attachment","scroll");}
 var apiKey  = 'NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1';
 var userID  = 'frederickrosales';
-
-//http://www.behance.net/v2/users/rikirosales?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
-//http://www.behance.net/v2/projects/475570?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
-//http://www.behance.net/v2/users/rikirosales/projects?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
-
-// Cache the Dom
-    window.scrollTo(0, 0);
-    dom = { 
-
-        window   : $(window),
-
-        document : $(document),
-
-        html     : $('html'),
-
-        body     : $('body')
-
-    };
-    dom.window.load(function(){
-
-        //Intro();
-        init();
-        loadMoreProject();
-        startNAv();
-        
-        //$('#page-preloader').fadeOut();
-
-
-    });
-
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    // from http://stackoverflow.com/a/11381730/989439
-    function mobilecheck() {
-        var check = false;
-        (function(a){
-            if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
-    }
-
-    var docElem = window.document.documentElement,
+var docElem = window.document.documentElement,
         //nav buttons
-        nav = Array.prototype.slice.call(document.querySelectorAll('nav > a') ),
+        nav = Array.prototype.slice.call(document.querySelectorAll('nav >ul>li> a') ),
         // support transitions
         support = Modernizr.csstransitions,
         // transition end event name
@@ -64,84 +21,105 @@ var userID  = 'frederickrosales';
         // click event (if mobile use touchstart)
         clickevent = mobilecheck() ? 'touchstart' : 'click';
 
-    function startNAv() {
-        var showMenu = document.getElementById( 'showMenu' ),
-            perspectiveWrapper = document.getElementById( 'perspective' ),
-            container = perspectiveWrapper.querySelector( '.perspective-container' ),
-            contentWrapper = container.querySelector( '.wrapper' );
 
-        showMenu.addEventListener( clickevent, function( ev ) {
-            ev.stopPropagation();
-            ev.preventDefault();
-            docscroll = scrollY();
-            // change top of contentWrapper
-            //contentWrapper.style.top = docscroll * -1 + 'px';
-            // mac chrome issue:
-           // document.body.scrollTop = document.documentElement.scrollTop = 0;
-            // add modalview class
-            classie.add( perspectiveWrapper, 'modalview' );
-            // animate..
-            setTimeout( function() { classie.add( perspectiveWrapper, 'animate' );classie.add( showMenu, 'hide' ); }, 25 );
-        });
+         var triggerBttn = document.getElementById( 'trigger-overlay' ),
+         container = document.querySelector('.perspective-container'),
+        overlay = document.querySelector( 'div.overlay1' ),
+        closeBttn = overlay.querySelector( 'button.overlay-close' );
+        console.log(closeBttn)
+        support = { transitions : Modernizr.csstransitions };
+//http://www.behance.net/v2/users/rikirosales?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
+//http://www.behance.net/v2/projects/475570?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
+//http://www.behance.net/v2/users/rikirosales/projects?callback=?&api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1
+
+// Cache the Dom
+    /*window.scrollTo(0, 0);
+    dom = { 
+
+        window   : $(window),
+
+        document : $(document),
+
+        html     : $('html'),
+
+        body     : $('body')
+
+    };
+    dom.window.load(function(){
+
+        //Intro();
+       
+        
+        //$('#page-preloader').fadeOut();
+
+
+    });*/
+     init();
+     loadMoreProject();
+     startNAv();
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    // from http://stackoverflow.com/a/11381730/989439
+    function mobilecheck() {
+        var check = false;
+        (function(a){
+            if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    }
+
+    
+
+    function startNAv() {
 
         nav.forEach(function(el,i){
             
             el.addEventListener(clickevent,function(ev){
-              
-                var onEndTransFn = function( ev ) {
-                    if( support && ( ev.target.className !== 'perspective-container' || ev.propertyName.indexOf( 'transform' ) == -1 ) ) return;
-                    this.removeEventListener( transEndEventName, onEndTransFn );
-                    classie.remove( perspectiveWrapper, 'modalview' );
-                    // mac chrome issue:
-                   // document.body.scrollTop = document.documentElement.scrollTop = docscroll;
-                    // change top of contentWrapper
-                    //contentWrapper.style.top = '0px';
-                };
-                if( support ) {
-                    perspectiveWrapper.addEventListener( transEndEventName, onEndTransFn );
-                }
-                else {
-                    onEndTransFn.call();
-                }
-
-                classie.remove( perspectiveWrapper, 'animate' );
-                 classie.remove( showMenu, 'hide' ); 
-                 console.log(ev.target.getAttribute('href'));
-                 setTimeout( 
-                    function() { 
-                        window.scrollTo(0,$(ev.target.getAttribute('href')).offset().top);
-                    }, 500 );
-                ev.preventDefault();
-                
+                window.scrollTo(0,$(ev.target.getAttribute('href')).offset().top);
+                 toggleOverlay()
             });
         });
         
-        function navigateTo(page){
-            
-                 
-        }
-        container.addEventListener( clickevent, function( ev ) {
-            if( classie.has( perspectiveWrapper, 'animate') ) {
-                var onEndTransFn = function( ev ) {
-                    if( support && ( ev.target.className !== 'perspective-container' || ev.propertyName.indexOf( 'transform' ) == -1 ) ) return;
-                    this.removeEventListener( transEndEventName, onEndTransFn );
-                    classie.remove( perspectiveWrapper, 'modalview' );
-                    // mac chrome issue:
-                    document.body.scrollTop = document.documentElement.scrollTop = docscroll;
-                    // change top of contentWrapper
-                    contentWrapper.style.top = '0px';
-                };
-                if( support ) {
-                    perspectiveWrapper.addEventListener( transEndEventName, onEndTransFn );
-                }
-                else {
-                    onEndTransFn.call();
-                }
-                classie.remove( perspectiveWrapper, 'animate' );
-            }
-        });
 
-        perspectiveWrapper.addEventListener( clickevent, function( ev ) { return false; } );
+
+
+   
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.add( overlay, 'close' );
+            classie.remove(triggerBttn,"active");
+            classie.remove(container,"hidden");
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add(triggerBttn,"active");
+            classie.add(container,"hidden");
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+
+
+
+
     }
 
 
@@ -176,7 +154,7 @@ function addScrollMagic() {
         offset: 100 /* offset the trigger 150px below #scene's top */
     })
     .setTween(tween)
-    .setClassToggle("#showMenu", "portfolio")
+    .setClassToggle("#trigger-overlay", "portfolio")
     .addTo(scrollMagicController);
 
 
@@ -196,7 +174,7 @@ function addScrollMagic() {
         offset: 150 /* offset the trigger 150px below #scene's top */
     })
     .setTween(tween2)
-    .setClassToggle("#showMenu", "about")
+    .setClassToggle("#trigger-overlay", "about")
     .addTo(scrollMagicController);
 
      scene = new ScrollScene({
@@ -204,7 +182,7 @@ function addScrollMagic() {
 
         offset: 150 /* offset the trigger 150px below #scene's top */
     })
-    .setClassToggle("#showMenu", "contact")
+    .setClassToggle("#trigger-overlay", "contact")
     .addTo(scrollMagicController);
 
 
@@ -282,8 +260,8 @@ function projectIDInit(projectID){
 }
 function loadMoreProject(event){
     var behanceProjectsAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key='+ apiKey;
-    $("#loadMore img").show();
-    $(".overlay").show();
+    //$("#loadMore img").show();
+   // $(".overlay").show();
     $.getJSON("json/projects.json", function(user) {
     //$.getJSON(behanceProjectsAPI, function(user) {
             
@@ -295,14 +273,14 @@ function loadMoreProject(event){
         })
      .done(function() {
         setTimeout( function(){
-            $("#loadMore ").hide();
-            $(".overlay").addClass("close");
+           // $("#loadMore ").hide();
+            //$(".overlay").addClass("close");
             $("img#profileImage").removeClass("riki"); 
 
             addScrollMagic();    
             $("body").removeClass("pageLoading");
              setTimeout( function(){$(".overlay").remove();},1000);
-               window.scrollTo(0,$(window.location.hash).offset().top);
+               //window.scrollTo(0,$(window.location.hash).offset().top);
         }, 2000 );
        
 
